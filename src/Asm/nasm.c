49ec7c90d576e548d4609b2f545ae6662c70c150
Tristan Erney <tristanerney@yahoo.com>
2024-01-07 23:05:32 -0500

    #3 create fix for file input

--- src/Asm/nasm.c
+++ src/Asm/nasm.c
@@ -29,29 +29,29 @@ int Nasm(
     char  _nasm_cmd[100] = { 0 };
     char  _nasm_fmt[] = "nasm -fbin %s -o %s";
 
-    sprintf(_nasm_cmd, _nasm_fmt, _input, _output);
-
-    FILE* f = fopen(_input, "wb");
-    if (!f) {
-        return 0;
-    }
+    sprintf(_nasm_cmd, _nasm_fmt, _input, _output); 
 
     if (code != NULL) {
+        FILE* f = fopen(_input, "wb+");
+        if (!f) {
+            return 0;
+        }
+
         char _bitmode_fmt[] = "bits %d\n";
         char _bitmode_buf[25] = { 0 };
         sprintf(_bitmode_buf, _bitmode_fmt, bit_mode);
         
         fwrite(_bitmode_buf, 1, strlen(_bitmode_buf), f);
         fwrite(code, 1, strlen(code), f);
+        fclose(f);
     }
-    fclose(f);
 
     retCode = system(_nasm_cmd);
     if (retCode != 0) {
         return 0;
     }
 
-    f = fopen(_output, "rb");
+    FILE* f = fopen(_output, "rb");
     fseek(f, 0, SEEK_END);
     size_t size = ftell(f);
     fseek(f, 0, SEEK_SET);
@@ -60,10 +60,16 @@ int Nasm(
     }
     fread(output, 1, size, f);
 
-    char _rm_fmt[] = "rm %s %s";
+    char _rm_fmt[] = "rm %s";
     char _rm_resources[100] = { 0 };
-    sprintf(_rm_resources, _rm_fmt, _input, _output);
+    sprintf(_rm_resources, _rm_fmt, _output);
     system(_rm_resources);
+
+    if (input == NULL) {
+        memset(_rm_resources, 0, 100);
+        sprintf(_rm_resources, _rm_fmt, _input);
+        system(_rm_resources);
+    }
 
     return size;
 }
